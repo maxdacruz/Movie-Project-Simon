69a84afe1853e7278ac1883b299aadde02def39d
maxdacruz <maxcruzlimla@gmail.com>
2020-03-30 16:33:56 +0200

    nav bar log out

--- nav-bar.php
+++ nav-bar.php
@@ -1,11 +1,16 @@
-
+<?php session_start() ?>
 <nav class="nav">
 
     <ul>
         <li><a href="./homepage.php">Home</a></li>
         <li><a href="./catalogue.php">Movies</a></li>
-        <li><a href="./login.php">Login</a></li>
-        <li><a href="./register.php">Register</a></li>
+        <?php if (!($_SESSION['logedin'])) {
+            echo '<li><a href="./login.php">Login</a></li>';
+            echo '<li><a href="./register.php">Register</a></li>';
+        } ?>
+        <?php if ($_SESSION['logedin']) {
+            echo '<li><a  href="#">Log Out</a></li>';
+        } ?>
 
 
     </ul>
