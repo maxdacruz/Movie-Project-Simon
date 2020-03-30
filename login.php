0e962ce8e9123edfad307512a332c3461accc441
maxdacruz <maxcruzlimla@gmail.com>
2020-03-30 10:04:59 +0200

    login register

---
+++ login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <main>
+    <form>
+      <label for="fname">First name:</label><br>
+      <input type="text" id="fname" name="fname"><br>
+      <label for="lname">Last name:</label><br>
+      <input type="text" id="lname" name="lname">
+      <label for="lname">Email:</label><br>
+      <input type="email" id="email" name="email">
+      <label for="psw">Password:</label><br>
+      <input type="password" id="psw" name="psw">
+      <label for="psw2">Repeat Password:</label><br>
+      <input type="password" id="psw2" name="psw2">
+    </form>
+  </main>
+</body>
+
+</html>
