e25bc91069c30ad6ad6546b0b4df3885e2a8af64
ADeSousa16 <lexdesousa@hotmail.com>
2020-03-30 16:32:02 +0200

    details page

--- details.php
+++ details.php
@@ -11,9 +11,24 @@
 </header>
 <?php 
 require_once("database.php");
-$movie_id = $_GET["id"];
-$query = "SELECT * FROM movies INNER JOIN category ON movies.director_id = directors.id  WHERE movies.id =" . $movid;
+$connect = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME);
+$movid = $_GET["id"];
+$query = "SELECT * 
+FROM movie
+INNER JOIN category ON movie.category_id = category.id 
+INNER JOIN actor ON movie.actor_id = actor.id
+WHERE movie.id =" . $movid;
+
+var_dump($query);
 if (isset($_GET['id'])){
+    $results = mysqli_query($connect, $query);
+    $movies = mysqli_fetch_assoc($results);
+    var_dump($movies);
+    echo '<h1>'. $movies['title']. '</h1>';
+    echo "<img width= 300 px src = " . $movies['poster'] . ">";
+    echo "<p>".$movies['first_name']. $movies['last_name']. "</p>";
+    echo "<p>".$movies['Synopsis']."</p>";
+    echo "<p>".$movies['category']."</p>";
 
 }
 
