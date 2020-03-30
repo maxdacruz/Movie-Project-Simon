40e1aa2d919f90f88631c4f58e1c42a41d6ad5d8
NoFreund <60601581+NoFreund@users.noreply.github.com>
2020-03-30 11:16:12 +0200

    nav update

--- nav-bar.php
+++ nav-bar.php
@@ -1,6 +1,9 @@
-<nav>
-  <a href="./">Home</a> |
-  <a href="./movies.php">Movies</a> |
-  <a href="./login.php">Login</a>
-  <a href="./register.php">Register</a>
-</nav>
+<nav class="nav">
+
+<ul>
+    <li><a href="./">Home</a></li>
+    <li><a href="./movies.php">Movies</a></li>
+    <li><a href="./login.php">Login</a></li>
+    <li><a href="./register.php">Register</a></li>
+    
+</ul>
