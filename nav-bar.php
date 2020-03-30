2c463498e53ea08a50c48ba313a97c339af846e1
maxdacruz <maxcruzlimla@gmail.com>
2020-03-30 13:12:55 +0200

    aze

--- nav-bar.php
+++ nav-bar.php
@@ -2,7 +2,7 @@
 
     <ul>
         <li><a href="./homepage.php">Home</a></li>
-        <li><a href="./movies.php">Movies</a></li>
+        <li><a href="./catalogue.php">Movies</a></li>
         <li><a href="./login.php">Login</a></li>
         <li><a href="./register.php">Register</a></li>
         <?php if ($_SESSION['logedin']) {
