edf3f4230ba952aa2739dc3497a319c70fe7fc2c
ADeSousa16 <lexdesousa@hotmail.com>
2020-03-30 12:03:14 +0200

    nav-bar name change + catalogue cut string

--- catalogue.php
+++ catalogue.php
@@ -12,7 +12,18 @@
         echo '<div class="card-body">';
         echo '<h2 class="card-title">' . $movie['title'] . "</h2>";
         echo '<p class="card-text">' . $movie['release_year'] . ".</p>";
-        echo '<p class="card-text">' . $movie['Synopsis'] . ".</p>";
+        $string =  $movie['Synopsis'];
+        if (strlen($string) > 30) {
+            $trimstring = substr($string, 0, 30). ' <a href="#">...</a>';
+            } else {
+            $trimstring = $string;
+            }
+            
+        echo '<p class="card-text">' . $trimstring . ".</p>";
         echo '<a href class="details">Details</a> <br>'; 
         echo '<button class="addToPlaylist">Add to Playlist</button> <br>'; 
     }
+    //Out
+    
+
+   
