1ddd3ad724f3d5d89c03fe740ca2896b6789ffb2
NoFreund <60601581+NoFreund@users.noreply.github.com>
2020-03-30 11:17:57 +0200

    stylesheet

--- homepage.php
+++ homepage.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>IDK Movies</title>
+
+    <link rel="stylesheet" href="app/style/main.css">
 </head>
 <body>
     
