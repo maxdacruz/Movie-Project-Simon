4f0552f24cf7986074465e7d3eae8d3f1241c2b3
bumcello <60598428+bumcello@users.noreply.github.com>
2020-03-31 12:06:51 +0200

    working file

--- addmovie.php
+++ addmovie.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Add Movie/title>
+  <title>Add Movie</title>
 
   <link rel="stylesheet" href="app/style/imports/main.css">
 </head>
