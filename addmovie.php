601e23f13b9dbc940adf834096cfebfe8c62f28b
ADeSousa16 <lexdesousa@hotmail.com>
2020-03-31 15:11:13 +0200

    addmovie finished

--- addmovie.php
+++ addmovie.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -17,14 +18,33 @@
     <form method="Post">
       <label for="title">Title:</label><br>
       <input type="text" id="title" name="title"><br>
-      <label for="lname">Last name:</label><br>
-      <input type="text" id="lname" name="lname"><br>
-      <label for="lname">Email:</label><br>
-      <input type="email" id="email" name="email"><br>
-
-
-      <input type="submit" value="Send">
+      <label for="poster">Poster:</label><br>
+      <input type="text" id="poster" name="poster"><br>
+      <label for="category">Category:</label><br>
+      <input type="text" id="category" name="category"><br>
+      <label for="actor">Actor:</label><br>
+      <input type="text" id="actor" name="actor"><br>
+      <label for="date">Release Year:</label><br>
+      <input type="date" id="releaseyear" name="releaseyear"><br>
+      <label for="synopsis">Synopsis:</label><br>
+      <input type="text" id="synopsis" name="synopsis"><br>
+       <input type="submit" value="Add">
     </form>
+  <?php 
+  include_once("database.php");
+  
+  $connect = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME);
+  $title = $_POST['title'];
+  $poster =$_POST['poster'];
+  $category =$_POST['category'];
+  $actor =$_POST['actor'];
+  $releaseyear =$_POST['releaseyear'];
+  $synopsis = $_POST['synopsis'];
+  $addmovie = "INSERT INTO movie(title,poster,category_id,actor_id,release_year,synopsis) VALUES('" . $title . "','" . $poster . "', '1','2','". $releaseyear."','".$synopsis."')";
+  $result = mysqli_query($connect, $addmovie);
+  var_dump($addmovie);
+
+  ?>
     <div id="result"></div>
   </main>
   <!--   <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
