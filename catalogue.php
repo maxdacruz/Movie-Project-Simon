fc62c5a058fb2b38e56abd604ae78cbd4558db56
maxdacruz <maxcruzlimla@gmail.com>
2020-03-31 14:19:22 +0200

    added option

--- catalogue.php
+++ catalogue.php
@@ -10,9 +10,9 @@
 
 <body>
     <header>
-    <?php
-    include_once("nav-bar.php");
-    ?>
+        <?php
+        include_once("nav-bar.php");
+        ?>
     </header>
 
     <div>
@@ -41,8 +41,12 @@
     $prev = $limit - 5;
     $next = $limit + 5;
     $MoviesQuery = "SELECT * FROM movie ORDER BY release_year $order  LIMIT $limit,$next";
+    $playlists = "SELECT * FROM playlist where user_id =" . $_SESSION['userid'];
     $result = mysqli_query($connect, $MoviesQuery);
+    $result2 = mysqli_query($connect, $playlists);
     $movies = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+    $playlists = mysqli_fetch_all($result2, MYSQLI_ASSOC);
 
     echo '<section class="movies">';
     //pagination
@@ -52,9 +56,9 @@
     if ($limit >= 5) echo '<a href="catalogue?limit=' . $prev . '">back</a> ';
     echo '<a href="catalogue?limit=' . $next . '">next</a>';
     echo '</div>';
-    
+
     echo '<div class="grid">';
-    
+
     foreach ($movies as $movie) {
         echo '<div>';
         echo '<img src="' . $movie['poster'] . '" class="img-details">';
@@ -71,7 +75,15 @@
         echo '<p class="card-text">' . $trimstring . ".</p>";
         echo '<p><strong>Details </strong>' . '<br>' .
             '<a href="details.php?id=' . $movie['id'] . '">' . $movie['title'] . '</a></p>';
-        echo '<button class="addToPlaylist">Add to Playlist</button> <br> </div>';
+
+        echo '<label for="playlist">Choose a Playlist:</label>';
+        echo '<select id="playlist">';
+
+        foreach ($playlists as $playlist) {
+            echo '<option value="' . $playlist['id'] . ',' . $movie['id'] . '">' . $playlist['name'] . ' </option>';
+        };
+        echo '</select>';
+        echo '<input type="submit" value="Add to Playlist">';
         echo '</div>';
     }
 
@@ -80,6 +92,28 @@
 
     ?>
 
+    <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+    <script>
+        $(function() {
+            $('input[type="submit"]').click(function(e) {
+                console.log('test');
+                e.preventDefault();
+                $.ajax({
+                    url: 'addtoplaylist.php',
+                    type: 'post',
+                    data: {
+                        selected: $('#select option:selected').val()
+                    },
+                    success: function(result) {
+                        console.log('ok');
+                    },
+                    error: function(err) {
+                        console.log('notok');
+                    }
+                });
+            });
+        });
+    </script>
 </body>
 
 </html>
