a7262f43087b9fc2e98ba00c74a135a15a3393f0
maxdacruz <maxcruzlimla@gmail.com>
2020-03-31 09:20:48 +0200

    log in out fixed

--- logout.php
+++ logout.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$_SESSION['logedin'] = false;
+unset($_SESSION['logedin']);
 ?>
 
 <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
--- nav-bar.php
+++ nav-bar.php
@@ -4,11 +4,11 @@
     <ul>
         <li><a href="./homepage.php">Home</a></li>
         <li><a href="./catalogue.php">Movies</a></li>
-        <?php if (!($_SESSION['logedin'])) {
+        <?php if (!isset($_SESSION['logedin'])) {
             echo '<li><a href="./login.php">Login</a></li>';
             echo '<li><a href="./register.php">Register</a></li>';
         } ?>
-        <?php if ($_SESSION['logedin']) {
+        <?php if (isset($_SESSION['logedin'])) {
             echo '<li><a  href="./logout.php">Log Out</a></li>';
         } ?>
 
