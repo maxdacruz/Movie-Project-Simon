6a04fef1cf49f0a480e20a669912ca2ad77f0b85
maxdacruz <maxcruzlimla@gmail.com>
2020-03-31 16:05:51 +0200

    azeaze

--- addplaylist.php
+++ addplaylist.php
@@ -1,10 +1,15 @@
 <?php
 session_start();
-$result_query = array();
+
 require_once 'database.php';
 $connect = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME);
-//$MoviesQuery = "SELECT * FROM movie";
-$date = date('Y/m/d ', time());
-$addplaylist = "INSERT INTO playlist(user_id, date, name) VALUES('" . $_SESSION['userid'] . "','" . $date . "', '" . $_POST['name'] . "')";
-var_dump($addplaylist);
-$result = mysqli_query($connect, $addplaylist);
+
+if (!empty($_POST['name'])) {
+
+  $date = date('Y/m/d ', time());
+  $addplaylist = "INSERT INTO playlist(user_id, date, name) VALUES('" . $_SESSION['userid'] . "','" . $date . "', '" . $_POST['name'] . "')";
+  var_dump($addplaylist);
+  $result = mysqli_query($connect, $addplaylist);
+} else {
+  echo '<p>add a name</p>';
+}
--- playlist.php
+++ playlist.php
@@ -23,27 +23,37 @@
 
 
 
+
   $result_query = array();
   require_once 'database.php';
   $connect = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME);
   //$MoviesQuery = "SELECT * FROM movie";
 
   $playlistquery = "SELECT * FROM movie_playlist, movie, playlist where movie_id = movie.id and user_id =" . $_SESSION['userid'] . " GROUP BY playlist.id";
+  $getMovies = "SELECT * FROM  movie INNER JOIN  movie_playlist ON movie_playlist.movie_id = movie.id";
   $result = mysqli_query($connect, $playlistquery);
+  $result2 = mysqli_query($connect, $getMovies);
   $playlists = mysqli_fetch_all($result, MYSQLI_ASSOC);
+  $movies = mysqli_fetch_all($result2, MYSQLI_ASSOC);
+  var_dump($movies);
 
   foreach ($playlists as $playlist) {
     echo "<div id='result'>";
     echo '<h2>' . $playlist['name'] . "</h2>";
-    echo '<h3>' . $playlist['release_year'] . "</h3>";
+
+    echo ' <input type="submit" value="delete" name="delete"> ';
+    echo ' <input type="submit" value="edit"  name="edit"> ';
     echo "</div>";
   }
 
+
   ?>
+  <div id="result"></div>
   <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
   <script>
     $(function() {
-      $('input[type="submit"]').click(function(e) {
+
+      $('input[value="Add"]').click(function(e) {
         console.log('test');
         e.preventDefault();
         $.ajax({
@@ -52,7 +62,28 @@
           data: $('form').serialize(),
           success: function(result) {
             console.log('ok');
+
             window.location.href = "./playlist.php"
+            $('#result').html(result);
+          },
+          error: function(err) {
+            console.log('notok');
+            $('#result').html(result);
+          }
+        });
+      });
+      $('input[value="delete"]').click(function(e) {
+        console.log('delete');
+        e.preventDefault();
+        $.ajax({
+          url: 'addplaylist.php',
+          type: 'post',
+          data: $('form').serialize(),
+          success: function(result) {
+            console.log('ok');
+
+            //window.location.href = "./playlist.php"
+            $('#result').html(result);
           },
           error: function(err) {
             console.log('notok');
